86b58c94c6d3f2025e353a3f33b9416230aaef8c
Supriya2807 <lahadesupriya@gmail.com>
2016-06-19 15:16:45 +0530

    session4assign4
    Reverse of a String

---
+++ session4assign4.java
@@ -0,0 +1,18 @@
+package acadgild.assignments;
+
+public class session4assign4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringBuffer str = new StringBuffer("I like Java!");
+		
+		System.out.println("The original String is :"+str);
+
+		str.reverse();
+		
+		System.out.println("The reverse of the String is :"+str);
+		
+	}
+
+}
